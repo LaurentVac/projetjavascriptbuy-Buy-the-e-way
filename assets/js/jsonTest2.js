532431ca641b4302ff8c127a1946fb9d84cd72d3
Lendesz <lendesz@gmail.com>
2020-11-19 20:00:18 +0100

    Remplissage des card OK + "mise en page card

--- assets/js/jsonTest2.js
+++ assets/js/jsonTest2.js
@@ -6,44 +6,37 @@ fetch('assets/json/catalog.json')
 
 
 function loadInHTML(catalog){
-    console.log(catalog);
-    //console.log(catalog[0].produit0);
+
+    //console.log(catalog);
+
   
     // boucle sur catalog pour extraire les données et les implanter dan sles "card" générées dynamiquement
     catalog.forEach((object, index) => {
 
-      // récupération des différents éléments de la card - création des variables
-      
-      let cardName = document.getElementById('cardName');
-      let cardHeader = document.getElementById('cardHeader');
-      let cardImg = document.getElementById('cardImg');
-      let cardTitle = document.getElementById('cardTitle');
-      let cardSubtitle = document.getElementById('cardSubtitle');
-      let cardText = document.getElementById('cardText');
-      let cardTextMuted = document.getElementById('cardTextMuted');
-
-      let i=1;
-
       // clonage de la card example ... essaie d"'utiliser fonction fetch pour récupérer !!!!!!!!!!!!!!!!!!!!!!!
       clone = document.getElementById("cardToClone").cloneNode(true);
+
+      // attribution du nouvel id
       clone.id = catalog[index].id;
 
-      cardTitle.innerHTML = catalog[index].prix + "€";
+      // Déclaration varialbles 
+      let cardPrice = document.querySelector('.card-title');
+      let cardProduct = document.querySelector('.card-subTitle');
+      let cardPara = document.querySelector('.card-text');
+      let cardBtn = document.querySelector('.btn');
+      let cardRef = document.querySelector('.text-muted');
 
-      cardSubtitle.innerHTML = catalog[index].produit;
-
-      cardText.innerHTML = catalog[index].description;
-      cardText.style.height = "70px";
-      //cardText.id = cardText.id + "i";
-      cardTextMuted.innerHTML = "Réf: " + catalog[index].ref;
-      //cardTextMuted.id = cardTextMuted.id + "i";
-
-
+      // Insertion des données récupéré du json
+      cardPrice.innerHTML = catalog[index].prix + "€";
+      cardProduct.innerHTML = catalog[index].produit;
+      cardPara.innerHTML = catalog[index].description;
+      cardPara.style.height = "70px";
+      cardBtn.style.height = "50px";
+      cardRef.innerHTML = "Réf: " + catalog[index].ref;
 
       // ajout de la nouvelle card dans la di row "mainRow"
       document.getElementById('mainRow').appendChild (clone);
 
-      i++;
     });
 }
 
