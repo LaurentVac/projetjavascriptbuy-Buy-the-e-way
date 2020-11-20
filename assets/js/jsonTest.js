e40c8d5cfed8ec1fc546d065f86efb3cdbb4686a
StephaneC <stephane.cavillon@itech.fr>
2020-11-20 09:27:37 +0100

    création des listeners sur les boutons ok

--- assets/js/jsonTest.js
+++ assets/js/jsonTest.js
@@ -1,11 +1,38 @@
+//Déclaration des tableaux
+let panier = [];
+let catalog = [];
+
+
 fetch('assets/json/catalog.json')
   .then(response => response.json())
-  .then(catalog => {
-    loadInHTML(catalog);
-  })
+  .then(json => {catalog = json;
+    loadInHTML();
+  });
+
+function loadInHTML(){
+  console.log(catalog);
+};
+
+//accès au bouton
+let btnCartAdd = document.querySelectorAll('.card-body a')
+console.log(btnCartAdd);
+
+btnCartAdd.forEach(item => {console.log(this);
+  item.addEventListener('click', ()=>{
+    console.log(item);
+    let ref = item.getAttribute('data-ref');
+    console.log(ref);
+    addProductToCart(ref);
+  });
+});
 
 
-function loadInHTML(catalog){
-    console.log(catalog);
-    console.log(catalog[0].produit0);
-}
+function addProductToCart(a){
+  //boucler sur le catalogue pour trouver l'objet avec la bonne ref
+  for(const element of catalog){
+    if(element.id == a){
+      console.log('trouvé !');
+      //ajouter l'objet au panier
+    };
+  };
+};
