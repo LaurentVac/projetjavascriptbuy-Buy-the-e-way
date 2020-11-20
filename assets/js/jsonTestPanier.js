616768b009e37e67f6bfa3004f5b06b96fb25400
StephaneC <stephane.cavillon@itech.fr>
2020-11-20 10:30:08 +0100

    fonction ajout au panier OK

--- assets/js/jsonTestPanier.js
+++ assets/js/jsonTestPanier.js
@@ -13,15 +13,19 @@ function loadInHTML(){
   console.log(catalog);
 };
 
-//accès au bouton
+//accès au bouton des cartes 
+//éventuellement le selecteur à mettre à jour
 let btnCartAdd = document.querySelectorAll('.card-body a')
-console.log(btnCartAdd);
+// console.log(btnCartAdd);
 
+//ajout des listeners sur les boutons et récupération de l'attribut data-ref du bouton
 btnCartAdd.forEach(item => {console.log(this);
   item.addEventListener('click', ()=>{
     console.log(item);
+    //récupération de l'attribut data-ref du bouton
     let ref = item.getAttribute('data-ref');
     console.log(ref);
+    // appel de la fonction ajouter au panier
     addProductToCart(ref);
   });
 });
@@ -33,6 +37,10 @@ function addProductToCart(a){
     if(element.id == a){
       console.log('trouvé !');
       //ajouter l'objet au panier
+      panier.push(element);
+      console.log(panier);
+
+      //on pourra récuperer la length du tableau pour afficher le compteur à ce moment la 
     };
   };
 };
