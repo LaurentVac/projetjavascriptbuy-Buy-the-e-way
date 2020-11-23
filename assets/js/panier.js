fabf1d704258898ecf198a757c8c2a035d734d74
StephaneC <stephane.cavillon@itech.fr>
2020-11-23 12:12:40 +0100

    pause dans l'ajout panier

--- assets/js/panier.js
+++ assets/js/panier.js
@@ -27,7 +27,7 @@ function loadInCART() {
   function addProductToCart(a) {
     //si le panier est vide on ajoute forcement l'objet au panier
     if (panier.length == 0) {
-      for (const object of catalog) {
+      for (var object of catalog) {
         if (object.id == a) {
           console.log('ajout au panier !');
           //ajouter l'objet au panier
@@ -38,10 +38,13 @@ function loadInCART() {
     } else{
       // Si le panier n'est pas vide on cherche si l'object y est déjà et on y ajoute +1 en quantité
       for (const element of panier) {
+        console.log('element id ' + element.id +' - a '+ a);
         if (element.id == a) {
           console.log('quantité +1');
           element.quantity += 1;
           console.log(panier);
+          break;
+
         } else { // le problème c'est que lorsqu'il y a plusieurs objets dans le panier ça continue de boucler
           console.log('pas dans le panier on ajoute au panier !');
           for (const object of catalog) {
@@ -51,6 +54,7 @@ function loadInCART() {
               console.log(panier);
             };
           };
+          break;
         };
       };
     };
