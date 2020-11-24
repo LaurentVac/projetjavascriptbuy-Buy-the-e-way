b2343d9797503cbb1997e6ffd2420636e147bc83
laurentvac <laurentvac80@gmail.com>
2020-11-24 10:28:25 +0100

    pasfini

--- assets/js/catalogExtract.js
+++ assets/js/catalogExtract.js
@@ -4,20 +4,19 @@ var catalog = [];
 // récupération des données json via methode fetch
 fetch('assets/json/catalog.json')
     .then(response => response.json())
-    .then(json => {
+    .then(response => {
         //variable catalog récupère le json et loadInHtml sort les données pour les traiter
-        catalog = json;
-        loadInHTML();
-        loadInCART();
+        catalog = response
+        loadInHTML(catalog);
     })
 
 
-function loadInHTML() {
+function loadInHTML(catalog) {
 
     // boucle sur catalog pour extraire les données et les implanter dans les "card" générées dynamiquement
     catalog.forEach((object, index) => {
 
-        // clonage de la card example ... essaie d"'utiliser fonction fetch pour récupérer !!!!!!!!!!!!!!!!!!!!!!!
+        // clonage de la card example 
         let cardLayout = document.getElementById('cardLayout');
 
         // Déclaration varialbles 
@@ -57,19 +56,152 @@ function loadInHTML() {
         cardRef.innerHTML = "Réf: " + catalog[index].ref;
         cardRef.style.fontSize = "20px";
 
+        // ajout attribut data-ref au bouton "J'achète"
+        cardBtn.setAttribute('data-ref', catalog[index].id);
+        cardBtn.id = "btn" + catalog[index].id;
+                
         // clonage de la card
         clone = cardLayout.cloneNode(true);
         // attribution nouvel id
         clone.id = catalog[index].id;
-        // ajout attribut data-ref au bouton "J'achète"
-        cardBtn.setAttribute('data-ref', clone.id);
+        
+        
 
         // ajout de la nouvelle card dans div row "mainRow"
         document.getElementById('cardRow').appendChild(clone);
 
     });
 
-    //On masque la card example
-    cardLayout.classList.add('d-none');
-
+    //cardLayout.classList.add('d-none');
 };
+
+
+var cart = [];
+
+function addArticleInCart(id) {
+ 
+    
+    let getId = id.replace('btn','').trim();
+    //console.log("id",getId);
+
+    catalog.forEach((item, index) => {
+        
+        if (getId == catalog[index].id) {
+           // console.log("getId",catalog[index].id);
+
+            if (cart.indexOf(catalog[index]) == -1) {
+                //console.log("catalog",catalog[index]);
+                cart.push(catalog[index]);
+                //console.log("cart",cart);
+            } else {
+               let getIndex = cart.indexOf(catalog[index]);
+               //console.log('index',getIndex)
+               cart[getIndex].quantity += 1;
+               console.log('qté',cart[getIndex])
+            }
+        }
+
+    })
+};
+
+
+let cartIn = document.getElementById('cartIn');
+cartIn.onclick = function() {
+
+    let deleteModal = document.getElementById('mainModal');
+    deleteModal.innerHTML = "";
+
+    cart.forEach((item, index) => {
+
+        console.log("cart",cart);
+
+        let exampleModal = document.getElementById('exampleObject');
+
+        let picture = exampleModal.querySelector('.modal-img');
+        picture.src = cart[index].picture;
+        console.log("picture", picture);
+
+        let orderTitle = exampleModal.querySelector('.modal-title');
+        orderTitle.innerHTML = cart[index].product;
+
+        var price = exampleModal.querySelector('.modal-price');
+        price.innerHTML = (cart[index].price * cart[index].quantity) + '€';
+
+        let quantity = exampleModal.querySelector('.modal-quantity');
+        quantity.innerHTML = cart[index].quantity;
+
+        let moreQuantity = exampleModal.querySelector('.btn2');
+        moreQuantity.id = 'btnMore' + cart[index].id;
+        let lessQuantity = exampleModal.querySelector('.btn1');
+        lessQuantity.id = 'btnLess' + cart[index].id;
+
+        // clone.id = cart[index].id;
+        clone = document.querySelector('#exampleObject').cloneNode(true);
+        clone.id = cart[index].id;
+
+        document.getElementById('mainModal').appendChild(clone);
+
+
+    });
+
+    // let deleteModal = document.getElementById('mainModal');
+    // deleteModal.innerHTML = "";
+    // if (deleteModal.innerHTML != "") {
+    //     console.log("delete modal");
+        
+    // }
+
+}
+
+
+function removeArticleQuantity(id) {
+    
+    let getId = id.replace('btnLess','').trim();
+    console.log("idless",getId);
+
+    cart.forEach((item, index) => {
+
+        let rowArticle = document.getElementById(getId);
+        let price = rowArticle.querySelector('.modal-price');
+        let quantity = rowArticle.querySelector('.modal-quantity');
+        
+        
+        if (getId == cart[index].id) {
+           console.log("getIdMore!!",cart[index].id);
+           if (cart[index].quantity > 0) {
+            cart[index].quantity -= 1;
+            console.log(cart[index].quantity,"qty");
+            quantity.innerHTML = cart[index].quantity;
+            price.innerHTML = (cart[index].price * cart[index].quantity) + '€';
+           }
+
+        }
+
+    })
+}
+
+function addArticleQuantity(id) {
+    
+    let getId = id.replace('btnMore','').trim();
+    //console.log("idless",getId);
+
+    cart.forEach((item, index) => {
+
+        let rowArticle = document.getElementById(getId);
+        let price = rowArticle.querySelector('.modal-price');
+        let quantity = rowArticle.querySelector('.modal-quantity');
+        
+        
+        if (getId == cart[index].id) {
+           console.log("getIdMore!!",cart[index].id);
+           if (cart[index].quantity != 0) {
+            cart[index].quantity += 1;
+            console.log(cart[index].quantity,"qty");
+            quantity.innerHTML = cart[index].quantity;
+            price.innerHTML = (cart[index].price * cart[index].quantity) + '€';
+           }
+
+        }
+
+    })
+}
--- assets/js/julien.js
+++ assets/js/julien.js
@@ -4,10 +4,13 @@
 
 let cartIn = document.getElementById('cartIn');
 cartIn.onclick = function() {
+    //création du panier 
 
+    //remise à 0 du panier
+    document.getElementById('mainModal').innerHTML = '';
+
+    // remplissage de la modal du panier pour toute les lignes du tableau panier
     panier.forEach((item, index) => {
-
-
 
         let picture = document.querySelector('#exampleObject .img-fluid');
         picture.src = panier[index].picture;
@@ -17,21 +20,86 @@ cartIn.onclick = function() {
         orderTitle.innerHTML = panier[index].product;
 
         let price = document.getElementById('price');
-        price.innerHTML = panier[index].price + '€';
+        price.setAttribute('data-ref', panier[index].id);
+        price.innerHTML = (panier[index].price * panier[index].quantity) + '€';
 
         let quantity = document.getElementById('quantity');
+        quantity.setAttribute('data-ref', panier[index].id);
         quantity.innerHTML = panier[index].quantity;
+
+        // ajout attribut data-ref au bouton "+"
+        let btnQtyAdd = document.querySelector('.qtyAdd');
+        btnQtyAdd.setAttribute('data-ref', panier[index].id);
+
+        // ajout attribut data-ref au bouton "-"
+        let btnQtyRemove = document.querySelector('.qtyRemove');
+        btnQtyRemove.setAttribute('data-ref', panier[index].id);
+
 
         clone = document.querySelector('#exampleObject').cloneNode(true);
         clone.id = panier[index].id;
 
         document.getElementById('mainModal').appendChild(clone);
-
-
     });
+    // fin de création du panier
 
-    let mask = document.getElementById('exampleObject');
-    mask.classList.add('d-none');
+        //accès au bouton des quantités 
+        //éventuellement le selecteur à mettre à jour
+        let btnQtyAdd = document.querySelectorAll('.qtyAdd');
+        let btnQtyRemove = document.querySelectorAll('.qtyRemove');
+        console.log(btnQtyAdd);
+        
+        // Création des listeners sur les btn +
+        btnQtyAdd.forEach( item => {
+          item.addEventListener('click', function (){
+            //aller chercher le data-ref du bouton
+            let ref = item.getAttribute('data-ref');
+        
+            //appel de la fonction pour ajouter une quantité
+            addQty(ref);
+          });
+        });
+        
+        // Création des listeners sur les btn -
+        btnQtyRemove.forEach( item => {
+            item.addEventListener('click', function removeQtyFromCart(){
+              //aller chercher le data-ref du bouton
+              let ref = item.getAttribute('data-ref');
+        
+              //appel de la fonction pour retirer une quantité
+              removeQty(ref);
+            });
+          });
 
+// fonction pour ajouter la quantité
+    function addQty(a){
+      //modifier la quantité de l'objet dans le panier
+      for(const element of panier){
+        if(element.id == a){
+          element.quantity += 1;
+          console.log(panier);            
+          //refaire les calculs et l'affichage de la quantité et le calcul
+          quantity.innerHTML = element.quantity;
+          price.innerHTML = (element.price * element.quantity) + '€';
+  // problème dans le renvoi de l'affichage par séléction de quantity et price
+        }
+      };
+    };
 
-}
+//fonction pour supprimer la quantité    
+    function removeQty(a){
+      //modifier la quantité de l'objet dans le panier
+      for(const element of panier){
+        if(element.id == ref){
+          element.quantity -= 1;
+          console.log(panier);
+
+          //refaire les calculs et l'affichage de lquantité et le calcul
+          quantity.innerHTML = element.quantity;
+          price.innerHTML = (element.price * element.quantity) + '€';
+  // problème dans le renvoi de l'affichage par séléction de quantity et price
+        
+        };
+      };
+    };
+ };
